9227bfce2705a764428ddcfccc75abe52f8d5db3
Shane <shaneabh777@gmail.com>
2022-07-03 23:12:05 +0400

    .

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import ShowSubscriber from './ShowSubscriber';
 import AddSubscriber from './AddSubsciber';
 import reportWebVitals from './reportWebVitals';
 import './Common/common.css'
+import PhoneDirectory from './PhoneDirectory';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <AddSubscriber />
-  </React.StrictMode>
+  
+    <PhoneDirectory />
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
